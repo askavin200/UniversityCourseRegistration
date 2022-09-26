4a7ba094560c4376dfd0e1e7442330db08f0b50d
afsaltk <85738212+afsaltk@users.noreply.github.com>
2022-09-26 17:17:03 +0530

    Update ICourseRepository.java
    Course Repository

--- UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/repo/ICourseRepository.java
+++ UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/repo/ICourseRepository.java
@@ -1,5 +1,11 @@
 package com.capgemini.UniversityCourseSelection.repo;
 
-public interface ICourseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.capgemini.UniversityCourseSelection.entities.Course;
+
+
+@Repository
+public interface ICourseRepo extends JpaRepository<Course, Integer> {
 
 }
