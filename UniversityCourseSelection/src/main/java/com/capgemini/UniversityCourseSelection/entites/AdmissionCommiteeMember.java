25dcb43e499ce7d2a61e0e38bd0be5881c5ab2dd
Adesh Medhe <66775833+adeshmedhe@users.noreply.github.com>
2022-09-27 10:18:13 +0530

    Update AdmissionCommiteeMember.java

--- UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/entites/AdmissionCommiteeMember.java
+++ UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/entites/AdmissionCommiteeMember.java
@@ -2,4 +2,54 @@ package com.capgemini.UniversityCourseSelection.entites;
 
 public class AdmissionCommiteeMember {
 
+	private int adminId;
+	private String adminName;
+	private String adminContact;
+	private String adminUsername;
+	private String adminPassword;
+
+	public AdmissionCommiteeMember() {
+		super();
+	}
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getAdminName() {
+		return adminName;
+	}
+
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+
+	public String getAdminContact() {
+		return adminContact;
+	}
+
+	public void setAdminContact(String adminContact) {
+		this.adminContact = adminContact;
+	}
+
+	public String getAdminUsername() {
+		return adminUsername;
+	}
+
+	public void setAdminUsername(String adminUsername) {
+		this.adminUsername = adminUsername;
+	}
+
+	public String getAdminPassword() {
+		return adminPassword;
+	}
+
+	public void setAdminPassword(String adminPassword) {
+		this.adminPassword = adminPassword;
+	}
+
 }
