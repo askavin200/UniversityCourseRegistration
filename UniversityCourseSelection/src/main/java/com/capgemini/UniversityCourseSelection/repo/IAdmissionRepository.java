7492ccc4612430c3ae81bf322eb2ef14d70ffe41
AnuragGuptagith <114135354+AnuragGuptagith@users.noreply.github.com>
2022-09-27 13:12:37 +0530

    Update IAdmissionRepository.java

--- UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/repo/IAdmissionRepository.java
+++ UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/repo/IAdmissionRepository.java
@@ -1,5 +1,12 @@
 package com.capgemini.UniversityCourseSelection.repo;
 
-public interface IAdmissionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.capgemini.UniversityCourseSelection.entities.Admission;
+
+@Repository
+public interface IAdmissionRepository extends JpaRepository<Admission,Integer> {
 
 }
+
