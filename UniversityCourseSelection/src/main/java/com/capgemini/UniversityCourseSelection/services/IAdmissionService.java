7929770fd45cc2946bb8d6d76b4a828b1e3093b1
AnuragGuptagith <114135354+AnuragGuptagith@users.noreply.github.com>
2022-09-27 14:19:38 +0530

    Update IAdmissionService.java

--- UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/services/IAdmissionService.java
+++ UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/services/IAdmissionService.java
@@ -7,7 +7,7 @@ import com.capgemini.UniversityCourseSelection.entities.Admission;
 public interface IAdmissionService {
 	
 	 	public Admission addAdmission(Admission add);
-	 	public Admission removeAdmission(int admissionId);
+	 	public void removeAdmission(int admissionId);
 	 	public Admission updateAdmission(Admission add);
 	 	public Admission viewAdmission(int admissionId);
 	 	public List<Admission> viewAllAdmission();
