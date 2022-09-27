4b83e9aca05ac3ffb555eb3b810803bb28084a2e
AnuragGuptagith <114135354+AnuragGuptagith@users.noreply.github.com>
2022-09-27 17:10:13 +0530

    Update AdmissionServiceImpl.java

--- UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/services/AdmissionServiceImpl.java
+++ UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/services/AdmissionServiceImpl.java
@@ -1,4 +1,5 @@
 package com.capgemini.UniversityCourseSelection.services;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.capgemini.UniversityCourseSelection.entities.Admission;
@@ -17,15 +18,14 @@ public class AdmissionServiceImpl implements IAdmissionService {
 	}
 
 	@Override
-	public Admission removeAdmission(int admissionId) {
+	public Admission cancelAdmission(int admissionId) {
 		Admission deletedadmission = null;
 		if(admissionrepo.existsById(admissionId)) {
 			deletedadmission = admissionrepo.findById(admissionId).get();
 			admissionrepo.deleteById(admissionId);
 			return deletedadmission;
 		}	
-		return deletedadmission;
-		
+		return deletedadmission;	
 	}
 
 	@Override
@@ -33,25 +33,20 @@ public class AdmissionServiceImpl implements IAdmissionService {
 		Admission updatedadmission = null;
 		if(admissionrepo.existsById(add.getCourseId())) 
 			updatedadmission = admissionrepo.save(add);
-		
 		return updatedadmission;
+	}	
+
+	@Override
+	public List<Admission> showAllAdmissionByCourseId(int courseId) {
+		List<Admission> admissionlist = admissionrepo.findAllAdmissionByCourseId(courseId);	
+		return admissionlist;
 	}
 
 	@Override
-	public Admission viewAdmission(int admissionId) {
-		Admission admission = null;
-		try {
-			admission = admissionrepo.findById(admissionId).get();
-		} catch (Exception e) {
-		}
-		
-		return admission;
-		
-	}
-
-	@Override
-	public List<Admission> viewAllAdmission() {
-		return admissionrepo.findAll() ;
+	public List<Admission> showAllAdmissionbyDate(LocalDate date) {
+		List<Admission> admissionlist = admissionrepo.findAllAdmissionByDate(date);	
+		return admissionlist;	
 	}
 
 }
+
