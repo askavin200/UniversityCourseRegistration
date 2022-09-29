72226e722fb2b9ae67174377ca58ded9ff255b33
Adesh Medhe <66775833+adeshmedhe@users.noreply.github.com>
2022-09-29 11:29:51 +0530

    Create AdmissionCommitteeMemberServiceImplTest.java

---
+++ UniversityCourseSelection/src/test/java/com/capgemini/UniversityCourseSelection/services/AdmissionCommitteeMemberServiceImplTest.java
@@ -0,0 +1,102 @@
+package com.capgemini.UniversityCourseSelection.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.capgemini.UniversityCourseSelection.entities.AdmissionCommiteeMember;
+import com.capgemini.UniversityCourseSelection.repo.IAdmissionCommiteeMemberRepository;
+
+@ExtendWith(MockitoExtension.class)
+class AdmissionCommitteeMemberServiceImplTest {
+
+	@Mock
+	private IAdmissionCommiteeMemberRepository repo;
+	
+	@InjectMocks
+	private AdmissionCommitteeMemberServiceImpl service;
+	
+	public AdmissionCommiteeMember mem = new AdmissionCommiteeMember(1,"adesh","1234123412","amedhe","pass");
+
+	
+	@Test
+	public void testAddCommitteeMember_success()
+	{
+		Mockito.when(repo.save(mem)).thenReturn(mem);
+		assertEquals(mem, service.addCommitteeMember(mem));
+	}
+	
+	@Test
+	public void testUpdateCommitteeMember_success()
+	{
+		Mockito.when(repo.save(mem)).thenReturn(mem);
+		Mockito.when(repo.existsById(mem.getAdminId())).thenReturn(true);
+		assertEquals(mem, service.updateCommitteeMember(mem));
+	}
+	
+	@Test
+	public void testViewCommitteeMember_success()
+	{
+		Mockito.when(repo.existsById(mem.getAdminId())).thenReturn(true);
+		Mockito.when(repo.findById(mem.getAdminId())).thenReturn(Optional.ofNullable(mem));
+		assertEquals(mem, service.viewCommitteeMember(1));
+	}
+	
+	@Test
+	public void testRemoveCommitteeMember_success()
+	{
+		Mockito.when(repo.existsById(1)).thenReturn(true);
+		boolean success = true;
+		try {
+			service.removeCommitteeMember(1);
+		}catch(Exception e)
+		{
+			success = false;
+		}
+		assertEquals(true,success);
+	}
+	
+	
+	@Test
+	public void testAddCommitteeMember_failure()
+	{
+		Mockito.when(repo.save(mem)).thenThrow(new IllegalArgumentException());
+		assertThrows(IllegalArgumentException.class, () -> {
+			service.addCommitteeMember(mem);
+		});
+	}
+	
+	@Test
+	public void testUpdateCommitteeMember_failure()
+	{
+		Mockito.when(repo.existsById(1)).thenReturn(true);
+		Mockito.when(repo.save(mem)).thenThrow(new IllegalArgumentException());
+		assertThrows(IllegalArgumentException.class, () -> {
+			service.updateCommitteeMember(mem);
+		});
+	}
+	
+	@Test
+	public void testViewCommitteeMember_failure()
+	{
+		Mockito.when(repo.existsById(1)).thenThrow(new IllegalArgumentException());
+		assertThrows(IllegalArgumentException.class, () -> {
+			service.viewCommitteeMember(1);
+		});
+	}
+
+	@Test
+	public void testRemoveCommitteeMember_failure()
+	{
+		Mockito.when(repo.existsById(1)).thenThrow(new IllegalArgumentException());
+		assertThrows(IllegalArgumentException.class, () -> {
+			service.removeCommitteeMember(1);
+		});
+	}
+}
