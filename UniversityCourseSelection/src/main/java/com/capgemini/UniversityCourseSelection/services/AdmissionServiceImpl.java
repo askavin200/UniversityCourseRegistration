f01d02ad244b5e8f693176047a8d7e512bc34a5f
AnuragGuptagith <114135354+AnuragGuptagith@users.noreply.github.com>
2022-10-01 12:42:20 +0530

    Update AdmissionServiceImpl.java

--- UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/services/AdmissionServiceImpl.java
+++ UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/services/AdmissionServiceImpl.java
@@ -7,15 +7,30 @@ import org.springframework.stereotype.Component;
 import com.capgemini.UniversityCourseSelection.entities.Admission;
 import com.capgemini.UniversityCourseSelection.exception.NotFoundException;
 import com.capgemini.UniversityCourseSelection.repo.IAdmissionRepository;
+import com.capgemini.UniversityCourseSelection.repo.IApplicantRepository;
+import com.capgemini.UniversityCourseSelection.repo.ICourseRepository;
 
 @Component
 public class AdmissionServiceImpl implements IAdmissionService {
 	
 	@Autowired
+	private ICourseRepository courseRepo;
+	
+	@Autowired
 	private IAdmissionRepository admissionrepo;
+	
+	@Autowired
+	private IApplicantRepository apprepo;
 	
 	@Override
 	public Admission addAdmission(Admission add) {
+		
+		if(!apprepo.existsById(add.getApplicantId())) {
+			throw new NotFoundException();
+		}
+		if(!courseRepo.existsById(add.getCourseId()))
+			throw new NotFoundException("Course not found");
+		
 		Admission addadmission = add;
 		return admissionrepo.save(addadmission);
 	}
@@ -34,6 +49,14 @@ public class AdmissionServiceImpl implements IAdmissionService {
 	@Override
 	public Admission updateAdmission(Admission add) {
 		Admission updatedadmission = null;
+		
+		if(!apprepo.existsById(add.getApplicantId())) {
+			throw new NotFoundException();
+		}
+		
+		if(!courseRepo.existsById(add.getCourseId()))
+			throw new NotFoundException("Course not found");
+		
 		if(add==null||!admissionrepo.existsById(add.getAdmissionId())) {
 			throw new NotFoundException();
 		}
